6da1923e77fe215a988928e6fa6f272cc20f699f
jperl <jperl@foundops.com>
2011-12-16 15:01:15 -0500

    Commented out LocationFieldValueRequired (there is a bug)

--- Source/Client/FoundOps.SLClient.Data/Validation/FieldRequired.cs
+++ Source/Client/FoundOps.SLClient.Data/Validation/FieldRequired.cs
@@ -1,34 +1,35 @@
-﻿using EntityGraph.Validation;
+﻿using EntityGraph.RIA.Validation;
+using EntityGraph.Validation;
 using FoundOps.Core.Models.CoreEntities;
 using System.ComponentModel.DataAnnotations;
 
 namespace FoundOps.SLClient.Data.Validation
 {
-    /// <summary>
-    /// Requires a LocationField to have a value if the field is required.
-    /// </summary>
-    public class LocationFieldValueRequired : ValidationRule<ValidationResult>
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LocationFieldValueRequired"/> class.
-        /// </summary>
-        public LocationFieldValueRequired()
-            : base(InputOutput<Field, bool>(field => field.Required), InputOutput<LocationField, Location>(field => field.Value))
-        {
-        }
+    ///// <summary>
+    ///// Requires a LocationField to have a value if the field is required.
+    ///// </summary>
+    //public class LocationFieldValueRequired : ValidationRule
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of the <see cref="LocationFieldValueRequired"/> class.
+    //    /// </summary>
+    //    public LocationFieldValueRequired()
+    //        : base(InputOutput<LocationField, Location>(field => field.Value))
+    //    {
+    //    }
 
-        /// <summary>
-        /// Validates the specified location field.
-        /// </summary>
-        /// <param name="fieldIsRequired">if set to <c>true</c> [field is required].</param>
-        /// <param name="location">The location field's location.</param>
-        /// <returns></returns>
-        public ValidationResult Validate(bool fieldIsRequired, Location location)
-        {
-            if (fieldIsRequired || location != null)
-                return ValidationResult.Success;
+    //    /// <summary>
+    //    /// Validates the specified location field.
+    //    /// </summary>
+    //    /// <param name="required">if set to <c>true</c> [field is required].</param>
+    //    /// <param name="value">The Location field's value</param>
+    //    /// <returns></returns>
+    //    public ValidationResult Validate(Location value)
+    //    {
+    //        if (value != null)
+    //            return ValidationResult.Success;
 
-            return new ValidationResult("The location is required.");
-        }
-    }
+    //        return new ValidationResult("The location is required.");
+    //    }
+    //}
 }
