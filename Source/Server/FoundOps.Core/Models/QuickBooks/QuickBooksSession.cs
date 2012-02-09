53d75b7da8069c6c70dcb0fe41397cd1a9156c55
zbright <zbright@foundops.com>
2012-02-09 16:51:43 -0500

    Added AES encryption/decryption to QBTokenSecret

--- Source/Server/FoundOps.Core/Models/QuickBooks/QuickBooksSession.cs
+++ Source/Server/FoundOps.Core/Models/QuickBooks/QuickBooksSession.cs
@@ -1,7 +1,19 @@
-﻿namespace FoundOps.Core.Models.QuickBooks
+﻿using FoundOps.Common.NET;
+using System.ServiceModel.DomainServices.Server;
+
+namespace FoundOps.Core.Models.QuickBooks
 {
     public class QuickBooksSession
     {
+        [Exclude]
+        private const string Password = "foosball27";
+
+        [Exclude] 
+        private const string InitialVector = "1Q@z32mvFeqwP~0D";
+
+        [Exclude] 
+        private const string Salt = "goose83";
+
         //The Id of the QuickBooks Account that is being accessed
         public string RealmId { get; set; }
 
@@ -16,7 +28,22 @@
         public string OAuthRealm { get; set; }
         public string OAuthSessionHandle { get; set; }
         public string OAuthToken { get; set; }
-        public string OAuthTokenSecret { get; set; }
+
+        //Used for serialization
+        public string OAuthTokenSecretEncrypted { get; set; }
+
+        [Exclude]
+        public string OAuthTokenSecret
+        {
+            get
+            {
+                return AESEncryption.Decrypt(OAuthTokenSecretEncrypted, Password, Salt, "SHA1", 2, InitialVector, 256);
+            }
+            set
+            {
+                OAuthTokenSecretEncrypted = AESEncryption.Encrypt(value, Password, Salt, "SHA1", 2, InitialVector, 256);
+            }
+        }
 
         //Token used by OAuth and QuickBooks to verify login
         public string QBToken { get; set; }
