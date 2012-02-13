d8a999456daf9baaae9e7ec77c448cfb8500afcb
zbright <zbright@foundops.com>
2012-02-13 13:03:35 -0500

    Fixed issue #8 - Now when you delete a Business account, its corresponding Azure Table will also be deleted

--- Source/Server/FoundOps.Server/Services/CoreDomainService/CoreDomainService.Core.cs
+++ Source/Server/FoundOps.Server/Services/CoreDomainService/CoreDomainService.Core.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Data.Objects;
 using FoundOps.Common.NET;
 using System.Collections.Generic;
+using FoundOps.Core.Models.QuickBooks;
 using FoundOps.Core.Server.Blocks;
 using FoundOps.Server.Controllers;
 using System.Security.Authentication;
@@ -144,6 +145,8 @@ namespace FoundOps.Server.Services.CoreDomainService
             businessAccountToDelete.Invoices.Load();
             foreach (var invoice in businessAccountToDelete.Invoices.ToList())
                 this.DeleteInvoice(invoice);
+
+            QuickBooksTools.DeleteAzureTable(businessAccountToDelete.Id);
         }
 
         #endregion
