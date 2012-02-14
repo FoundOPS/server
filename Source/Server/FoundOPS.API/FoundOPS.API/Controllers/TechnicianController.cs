6c859705049df5aa245a7241db752f7c3e13b307
jperl <jperl@foundops.com>
2012-02-13 22:49:57 -0500

    Setup FoundOPS API project. Setup first set of data tranfer objects (DTOs). Setup JSONP response

---
+++ Source/Server/FoundOPS.API/FoundOPS.API/Controllers/TechnicianController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using FoundOps.Common.NET;
+using FoundOps.Core.Models.CoreEntities;
+using FoundOps.Core.Models.CoreEntities.DesignData;
+
+namespace FoundOPS.API.Controllers
+{
+    /// <summary>
+    /// Holds the API for Technician information.
+    /// </summary>
+    [JsonpFilter] //Return Jsonp so other browsers accept cross domain requests
+    public class TechnicianController : Controller
+    {
+        private readonly LocationsDesignData _locationsDesignData = new LocationsDesignData();
+        //
+        // GET: /Technician/GetRoutes
+
+        public JsonResult GetRoutes()
+        {
+            var route = new Route();
+
+            var routeStop = new RouteDestination { Location = _locationsDesignData.DesignLocation, OrderInRoute = 1 };
+            route.RouteDestinations.Add(routeStop);
+            
+            routeStop = new RouteDestination { Location = _locationsDesignData.DesignLocationTwo, OrderInRoute = 2, };
+            route.RouteDestinations.Add(routeStop);
+
+            routeStop = new RouteDestination { Location = _locationsDesignData.DesignLocationThree, OrderInRoute = 3 };
+            route.RouteDestinations.Add(routeStop);
+
+            //Commented out because I do not think this is necessary
+            //HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*"); 
+
+            return Json(Models.Route.ConvertModel(route), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Source/Server/FoundOPS.API/FoundOPS.API/Models/Route.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace FoundOPS.API.Models
+{
+    public class Route
+    {
+        public string Name { get; set; }
+        public List<RouteDestination> RouteDestinations { get; set; } 
+
+        public Route()
+        {
+            RouteDestinations= new List<RouteDestination>();
+        }
+
+        public static Route ConvertModel(FoundOps.Core.Models.CoreEntities.Route routeModel)
+        {
+            var route = new Route {Name = routeModel.Name};
+         
+            foreach(var routeDestinationModel in routeModel.RouteDestinations)
+                route.RouteDestinations.Add(RouteDestination.ConvertModel(routeDestinationModel));
+
+            return route;
+        }
+    }
+}
