2e0e9083ea2a3bc8b2bf93a745093d59ba32631c
jperl <jperl@foundops.com>
2012-02-16 20:50:45 -0500

    Now returns multiple routes

--- Source/Server/FoundOPS.API/FoundOPS.API/Controllers/TechnicianController.cs
+++ Source/Server/FoundOPS.API/FoundOPS.API/Controllers/TechnicianController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using FoundOps.Common.NET;
 using FoundOps.Core.Models.CoreEntities;
 using FoundOps.Core.Models.CoreEntities.DesignData;
@@ -17,21 +19,29 @@ namespace FoundOPS.API.Controllers
 
         public JsonResult GetRoutes()
         {
-            var route = new Route();
+            var routes = new List<Route>();
+            for (int i = 0; i < 20; i++)
+            {
+                var route = new Route {Name = "What up"};
 
-            var routeStop = new RouteDestination { Location = _locationsDesignData.DesignLocation, OrderInRoute = 1 };
-            route.RouteDestinations.Add(routeStop);
-            
-            routeStop = new RouteDestination { Location = _locationsDesignData.DesignLocationTwo, OrderInRoute = 2, };
-            route.RouteDestinations.Add(routeStop);
+                var routeStop = new RouteDestination {Location = _locationsDesignData.DesignLocation, OrderInRoute = 1};
+                route.RouteDestinations.Add(routeStop);
 
-            routeStop = new RouteDestination { Location = _locationsDesignData.DesignLocationThree, OrderInRoute = 3 };
-            route.RouteDestinations.Add(routeStop);
+                routeStop = new RouteDestination {Location = _locationsDesignData.DesignLocationTwo, OrderInRoute = 2,};
+                route.RouteDestinations.Add(routeStop);
+
+                routeStop = new RouteDestination {Location = _locationsDesignData.DesignLocationThree, OrderInRoute = 3};
+                route.RouteDestinations.Add(routeStop);
+
+                routes.Add(route);
+            }
+
+            //Convert the routes to the API model route
+            var apiRoutes = routes.Select(Models.Route.ConvertModel).ToList();
 
             //Commented out because I do not think this is necessary
-            //HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*"); 
-
-            return Json(Models.Route.ConvertModel(route), JsonRequestBehavior.AllowGet);
+                //HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*"); 
+            return Json(apiRoutes, JsonRequestBehavior.AllowGet);
         }
     }
 }
