a8dbc2f8fdfc824b9f5704c3942ff10e9c87c45e
jperl <jperl@foundops.com>
2012-04-18 20:48:15 -0400

    Fixed crashing issue by switching to ClassicPipeline mode. http://stackoverflow.com/questions/1495144/net-2-0-help-troubleshooting-a-system-accessviolationexception-100-managed-c

---
+++ Source/Server/FoundOps.Server/WebRole.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Web.Administration;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using System.Linq;
+
+namespace FoundOps.Server
+{
+    /// <summary>
+    /// Used to force the project into Classic mode to prevent crashing issues due to parallel domain services.
+    /// http://stackoverflow.com/questions/1495144/net-2-0-help-troubleshooting-a-system-accessviolationexception-100-managed-c
+    /// </summary>
+    public class WebRole : RoleEntryPoint
+    {
+        public override bool OnStart()
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var sitesForInstance = serverManager.Sites.Where(s => s.Name.Contains(RoleEnvironment.CurrentRoleInstance.Role.Name));
+                var poolsForInstance = serverManager.ApplicationPools.Where(pool => sitesForInstance.Any(s => s.Applications.Any(a => a.ApplicationPoolName == pool.Name)));
+
+                foreach(var pool in poolsForInstance)
+                    pool.ManagedPipelineMode = ManagedPipelineMode.Classic;
+                
+                serverManager.CommitChanges();
+            }
+
+            return base.OnStart();
+        }
+    }
+}
