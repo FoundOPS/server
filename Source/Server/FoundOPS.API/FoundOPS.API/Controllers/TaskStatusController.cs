4db24d95941bd3224890b254d97976307202b1a0
zbright <zbright@foundops.com>
2012-05-08 18:51:39 -0400

    Added CRUD functionality to the API for TaskStatus'

---
+++ Source/Server/FoundOPS.API/FoundOPS.API/Controllers/TaskStatusController.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using System.ServiceModel.DomainServices.EntityFramework;
+using FoundOps.Common.NET;
+using FoundOps.Core.Models.CoreEntities;
+using TaskStatus = FoundOPS.API.Models.TaskStatus;
+using System.Web.Http;
+
+namespace FoundOPS.API.Controllers
+{
+#if !DEBUG
+    [Authorize]
+#endif
+
+    /// <summary>
+    /// An API controller that exposes the Task Status'
+    /// </summary>
+    public class TaskStatusController : ApiController
+    {
+        private readonly CoreEntitiesContainer _coreEntitiesContainer = new CoreEntitiesContainer();
+
+        #region INSERT
+
+        public void InsertTaskStatus(TaskStatus taskStatus)
+        {
+            var status = TaskStatus.ConvertModel(taskStatus);
+
+            if (status.EntityState == EntityState.Detached)
+                _coreEntitiesContainer.ObjectStateManager.ChangeObjectState(status, EntityState.Added);
+            else
+                _coreEntitiesContainer.TaskStatus.AddObject(status);       
+        }
+
+        #endregion
+
+        #region UPDATE
+
+        public void UpdateTaskStatus(TaskStatus taskStatus)
+        {
+            _coreEntitiesContainer.TaskStatus.AttachAsModified(TaskStatus.ConvertModel(taskStatus));
+        }
+
+        #endregion
+
+        #region DELETE
+
+        public void DeleteTaskStatus(TaskStatus taskStatus)
+        {
+            var status = TaskStatus.ConvertModel(taskStatus);
+
+            _coreEntitiesContainer.DetachExistingAndAttach(status); 
+            _coreEntitiesContainer.TaskStatus.DeleteObject(status);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Server/FoundOPS.API/FoundOPS.API/Models/TaskStatus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoundOPS.API.Models
+{
+    public class TaskStatus
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public bool RoutedStatusBool { get; set; }
+        public bool CreationDefaultBool { get; set; }
+        public bool RoutedDefaultBool { get; set; }
+        public bool CompletedDefaultBool { get; set; }
+        public Guid BusinessAccountId { get; set; }
+
+        public static FoundOps.Core.Models.CoreEntities.TaskStatus ConvertModel(TaskStatus modelStatus)
+        {
+            var status = new FoundOps.Core.Models.CoreEntities.TaskStatus
+                             {
+                                 Name = modelStatus.Name,
+                                 Color = modelStatus.Color,
+                                 RoutedStatusBool = modelStatus.RoutedStatusBool,
+                                 CreationDefaultBool = modelStatus.CreationDefaultBool,
+                                 RoutedDefaultBool = modelStatus.RoutedDefaultBool,
+                                 CompletedDefaultBool = modelStatus.CompletedDefaultBool,
+                                 BusinessAccountId = modelStatus.BusinessAccountId
+                             };
+
+            return status;
+        }
+    }
+}
