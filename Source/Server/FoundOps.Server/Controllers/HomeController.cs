b27301167441fb9ba806e174fbabd3a90c267e0f
Jonathan Perl <jperl@foundops.com>
2012-07-09 20:15:17 -0400

    Navigator now showing up. Initial initializeData setup (needs cleaning up)

--- Source/Server/FoundOps.Server/Controllers/HomeController.cs
+++ Source/Server/FoundOps.Server/Controllers/HomeController.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
 using FoundOps.Core.Models;
+using FoundOps.Core.Models.Azure;
+using FoundOps.Core.Models.CoreEntities;
+using FoundOps.Core.Tools;
 using FoundOps.Server.Tools;
 using System;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Formatting = System.Xml.Formatting;
+
 #if !DEBUG //RELEASE or TESTRELEASE
 using System.IO;
 using System.Net;
@@ -12,21 +23,126 @@ namespace FoundOps.Server.Controllers
 {
     public class HomeController : Controller
     {
+        private CoreEntitiesContainer _coreEntitiesContainer = new CoreEntitiesContainer();
+
         //Cannot require the page to be HTTPS until we have our own tile server
         //#if !DEBUG
         //        [RequireHttps]
         //#endif
         public ActionResult Index()
         {
-#if DEBUG
-            if (ServerConstants.AutomaticLoginFoundOPSAdmin)
-                return View();
-#endif
-
+#if !DEBUG
             if (!HttpContext.User.Identity.IsAuthenticated)
                 return Redirect(Global.RootFrontSiteUrl);
+#endif
 
-            return View();
+            var user = _coreEntitiesContainer.CurrentUserAccount().Include(ua => ua.PartyImage)
+                        .Include(ua => ua.RoleMembership).Include("RoleMembership.Blocks").Include("RoleMembership.OwnerParty")
+                        .First();
+
+            //Load all of the party images for the owner's of roles
+            var businessOwnerIds = user.RoleMembership.Select(r => r.OwnerParty).OfType<BusinessAccount>().Select(ba => ba.Id).Distinct();
+            var partyImages = _coreEntitiesContainer.Files.OfType<PartyImage>().Where(pi => businessOwnerIds.Contains(pi.PartyId)).Distinct();
+            if (user.PartyImage != null)
+                partyImages = partyImages.Union(new[] { user.PartyImage });
+
+            //Go through each party image and get the url with the shared access key
+            var partyImageUrls = new Dictionary<Guid, string>();
+            foreach (var partyImage in partyImages)
+            {
+                var imageUrl = partyImage.RawUrl + AzureServerHelpers.GetBlobUrlHelper(partyImage.OwnerParty.Id, partyImage.Id);
+                partyImageUrls.Add(partyImage.OwnerParty.Id, imageUrl);
+            }
+
+            var roles = user.RoleMembership.Distinct().OrderBy(r => r.OwnerParty.DisplayName);
+            var sections = roles.SelectMany(r => r.Blocks).Distinct().OrderBy(b => b.Name);
+
+            //Build the initializeConfig model
+            var sb = new StringBuilder();
+            var sw = new StringWriter(sb);
+
+            using (JsonWriter jsonWriter = new JsonTextWriter(sw))
+            {
+#if DEBUG
+                jsonWriter.Formatting = Newtonsoft.Json.Formatting.Indented;
+#endif
+                jsonWriter.WriteStartObject(); //initializeConfig object
+                jsonWriter.WritePropertyName("name");
+                jsonWriter.WriteValue(user.FirstName + " " + user.LastName);
+
+                jsonWriter.WritePropertyName("avatarUrl");
+                jsonWriter.WriteValue(user.PartyImage != null
+                                          ? partyImageUrls[user.PartyImage.Id]
+                                          : "img/emptyPerson.png");
+
+                jsonWriter.WritePropertyName("roles");
+                jsonWriter.WriteStartArray();
+
+                //Go through each of the user's roles
+                foreach (var role in roles)
+                {
+                    jsonWriter.WriteStartObject(); //start role object
+
+                    //Set the id
+                    jsonWriter.WritePropertyName("id");
+                    jsonWriter.WriteValue(role.Id);
+
+                    //Set the business account name
+                    jsonWriter.WritePropertyName("name");
+                    jsonWriter.WriteValue(role.OwnerParty.DisplayName);
+
+                    //Set the business's logo
+                    jsonWriter.WritePropertyName("businessLogoUrl");
+                    jsonWriter.WriteValue(role.OwnerParty.PartyImage != null
+                                              ? partyImageUrls[role.OwnerParty.PartyImage.Id]
+                                              : "img/emptyBusiness.png");
+
+                    //Add the available sections's names for the roles
+                    jsonWriter.WritePropertyName("sections");
+                    jsonWriter.WriteStartArray();
+                    foreach(var section in role.Blocks)
+                    {
+                        jsonWriter.WriteValue(section.Name);
+                    }
+                    jsonWriter.WriteEnd(); //end sections array
+                    jsonWriter.WriteEnd(); //end role object
+                }
+
+                jsonWriter.WriteEnd(); //end roles array
+
+
+                //Add each of the available blocks (sections) and their details
+                jsonWriter.WritePropertyName("sections");
+                jsonWriter.WriteStartArray();
+                foreach(var section in sections)
+                {
+                    jsonWriter.WriteStartObject(); //start section object
+
+                    jsonWriter.WritePropertyName("name");
+                    jsonWriter.WriteValue(section.Name);
+
+                    //TODO add these to DB
+                    jsonWriter.WritePropertyName("url");
+                    jsonWriter.WriteValue("#Employees");
+
+                    jsonWriter.WritePropertyName("color");
+                    jsonWriter.WriteValue("red");
+
+                    jsonWriter.WritePropertyName("iconUrl");
+                    jsonWriter.WriteValue("img/employees.png");
+
+                    jsonWriter.WriteEnd(); //end section object
+                }
+                jsonWriter.WriteEnd(); //end section array
+
+                jsonWriter.WriteEndObject(); //end initializeConfig object
+            }
+
+            var configData = sb.ToString();
+
+            //Cast to object so the overload for model data is used
+            //instead of it being confused as the view name
+            return View((object)configData);
         }
 
         //Cannot require the page to be HTTPS until we have our own tile server
