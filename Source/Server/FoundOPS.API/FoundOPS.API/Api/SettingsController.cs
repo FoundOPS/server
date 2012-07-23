1efe7df2122d11dbcafd01c0ebacca2b898844be
apohl1111 <apohl@foundops.com>
2012-07-23 11:34:13 -0400

    add fix for editing linked employee

--- Source/Server/FoundOPS.API/FoundOPS.API/Api/SettingsController.cs
+++ Source/Server/FoundOPS.API/FoundOPS.API/Api/SettingsController.cs
@@ -345,8 +345,9 @@ namespace FoundOPS.API.Api
 
             //Create the link that will login the new user and then redirect them to the
             //settings page where they can change their password
-            var redirect = ServerConstants.RootApplicationUrl + "/navigator.html#view/createPassword.html";
-            var link = ServerConstants.RootApiUrl + "/api/Helper/Login?email=" + user.EmailAddress + "&pass=" + temporaryPassword + "&redirect=" + redirect;
+            var redirect = ServerConstants.RootApplicationUrl + "/navigator.html%23view/createPassword.html";
+            var link = ServerConstants.RootApiUrl + "/Account/Login?EmailAddress=" + user.EmailAddress + "&Password=" + temporaryPassword + "&returnUrl=" + redirect;
+            //http://localhost:31820/Account/Login?EmailAddress=jperl@foundops.com&Password=seltzer&returnUrl=http://localhost:8000/navigator.html%23view/createPassword.html
 
             //Construct the email
             var from = _coreEntitiesContainer.CurrentUserAccount().First().EmailAddress;
@@ -429,7 +430,7 @@ namespace FoundOPS.API.Api
                 user.LinkedEmployees.Remove(employee);
             }
             //Changing a user from no employee to an employee
-            else if (employee == null && settings.Employee != null)
+            else if (employee == null && settings.Employee != null && settings.Employee.FirstName != "None")
             {
                 AddNewEmployeeToUser(settings, user, businessAccount); 
             }
