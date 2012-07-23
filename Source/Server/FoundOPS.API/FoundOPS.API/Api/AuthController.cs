20822bad8dfb6e642a1a5ef213ffdb37e12cdd52
Jonathan Perl <jperl@foundops.com>
2012-07-23 11:30:43 -0400

    Removed content includes, apparently they are not necessary (now no longer need to refresh). Added optional redirect url parameter on login. Removed helper controller action, kept it as a placeholder for other standard mvc controllers.

--- Source/Server/FoundOPS.API/FoundOPS.API/Api/AuthController.cs
+++ Source/Server/FoundOPS.API/FoundOPS.API/Api/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using FoundOps.Core.Models.Authentication;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using FoundOps.Core.Models.Authentication;
 using System.Web.Http;
 
 namespace FoundOPS.API.Api
@@ -17,16 +20,25 @@ namespace FoundOPS.API.Api
         /// </summary>
         /// <param name="email">The email address.</param>
         /// <param name="pass">The password.</param>
+        /// <param name="redirectUrl">(Optional) If specified, it will redirect here on success.</param>
         [System.Web.Http.AcceptVerbs("GET", "POST")]
-        public bool Login(string email, string pass)
+        public HttpResponseMessage Login(string email, string pass, string redirectUrl = "")
         {
             if (_membershipService.ValidateUser(email, pass))
             {
                 _formsService.SignIn(email, false);
-                return true;
+
+                if (!string.IsNullOrEmpty(redirectUrl))
+                {
+                    var response = Request.CreateResponse(HttpStatusCode.Moved);
+                    response.Headers.Location = new Uri(redirectUrl);
+                    return response;
+                }
+
+                return Request.CreateResponse(HttpStatusCode.Accepted, true);
             }
 
-            return false;
+            return Request.CreateResponse(HttpStatusCode.Unauthorized, false);
         }
 
         /// <summary>
