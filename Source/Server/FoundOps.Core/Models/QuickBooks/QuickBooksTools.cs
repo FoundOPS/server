4cc9282592b6a4097504523ccffa6b8275309db3
zbright <zbright@foundops.com>
2012-09-13 11:31:47 -0400

    Got authorization working again

--- Source/Server/FoundOps.Core/Models/QuickBooks/QuickBooksTools.cs
+++ Source/Server/FoundOps.Core/Models/QuickBooks/QuickBooksTools.cs
@@ -30,12 +30,12 @@ namespace FoundOps.Core.Models.QuickBooks
             //AKA oauthLink
             public const string AuthorizeUrl = "https://workplace.intuit.com/Connect/Begin";
 
-            public static readonly string GrantUrl = "https://localhost:44300/QuickBooks/OAuthGrantLogin";
+            public static readonly string GrantUrl = "http://localhost:31820/QuickBooks/OAuthGrantLogin";
 
             public static readonly string ConsumerKey = ConfigurationManager.AppSettings["consumerKey"];
             public static readonly string ConsumerSecret = ConfigurationManager.AppSettings["consumerSecret"];
 
-            public static readonly string OauthCallbackUrl = "https://localhost:44300/QuickBooks/OAuthGrantHandler";
+            public static readonly string OauthCallbackUrl = "http://localhost:31820/QuickBooks/OAuthGrantHandler";
 
             //Connection String for Azure Tables
             public const string StorageConnectionString = "DefaultEndpointsProtocol=http;AccountName=fstorequickbooks;AccountKey=fyZ0dsbFfQZET9RFdIlSeVepYgmtO0aBQYVArhazF0KO2X80BUZ2drEJLmRYjDbzelf7PAKzTrePzMJpt3vGaA==";
