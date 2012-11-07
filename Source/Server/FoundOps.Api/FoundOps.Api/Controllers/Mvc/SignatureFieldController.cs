58b06b94826690c20aa8ae227a95623d4bd8a47b
jperl <jperl@foundops.com>
2012-11-07 13:36:01 -0500

    setup signature field on api to work with jsignature

--- Source/Server/FoundOps.Api/FoundOps.Api/Controllers/Mvc/SignatureFieldController.cs
+++ Source/Server/FoundOps.Api/FoundOps.Api/Controllers/Mvc/SignatureFieldController.cs
@@ -1,7 +1,5 @@
-﻿using ConsumedByCode.SignatureToImage;
-using FoundOps.Core.Models.CoreEntities;
+﻿using FoundOps.Core.Models.CoreEntities;
 using System;
-using System.Drawing;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -10,21 +8,22 @@ namespace FoundOps.Api.Controllers.Mvc
     public class SignatureFieldController : Controller
     {
         /// <summary>
-        /// Shows the signature image of a signature field
+        /// Return the signature svg of a signature field
         /// </summary>
-        /// <param name="signatureId">The id of the signature field</param>
+        /// <param name="id">The id of the signature field</param>
         /// <returns>An image of the signature</returns>
-        public Bitmap GetSigntureImage(Guid signatureId)
+        public string Image(Guid id)
         {
             var coreEntitiesContainer = new CoreEntitiesContainer();
             
-            var signatureField = coreEntitiesContainer.Fields.OfType<SignatureField>().FirstOrDefault(sf => sf.Id == signatureId);
+            var signatureField = coreEntitiesContainer.Fields.OfType<SignatureField>().FirstOrDefault(sf => sf.Id == id);
             if (signatureField == null) return null;
 
-            var sigToImage = new SignatureToImage();
-            var signatureImage = sigToImage.SigJsonToImage(signatureField.Value);
-
-            return signatureImage;
+            //Data must be stored using https://github.com/brinley/jSignature base30 format
+            var data = new jSignature.Tools.Base30Converter().GetData(signatureField.Value);
+            
+            string svg = jSignature.Tools.SVGConverter.ToSVG(data);
+            return svg;
         }
     }
 }
